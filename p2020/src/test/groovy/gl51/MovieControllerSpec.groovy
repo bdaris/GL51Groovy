c9a093f5da26a6a3234b5076d74b957fb168492c
daris <darisborel5@gmail.com>
2020-04-12 13:14:38 +0200

    Mise en place des tests

--- p2020/src/test/groovy/gl51/MovieControllerSpec.groovy
+++ p2020/src/test/groovy/gl51/MovieControllerSpec.groovy
@@ -50,4 +50,10 @@ class MovieControllerSpec extends Specification {
         content.find { it.titre == 'mymovie' && it.imdbID == "aaaa" }
     }
 
+    @MockBean(MovieClientImpl)
+    MovieClient movieClient() {
+        def mock = Mock(MovieClient)
+        mock.getMovieDetail("aaaa") >> new Movie(imdbID: "aaaa", titre: 'mymovie')
+        mock
+    }
 }
